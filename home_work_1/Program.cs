038d6b83fa1f271456cc9a38412bf1df505fb44a
Limgraph <limgraph@mail.ru>
2023-02-04 01:08:11 +0300

    task_6 hw_1

--- home_work_1/Program.cs
+++ home_work_1/Program.cs
@@ -1,32 +1,15 @@
-﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
 Console.Clear();
 
-Console.Write("Введите первое число: ");
-int number_1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число для проверки на чётность: ");
+int x = int.Parse(Console.ReadLine()!);
 
-Console.Write("Введите второе число: ");
-int number_2 = int.Parse(Console.ReadLine()!);
-
-Console.Write("Введите третье число: ");
-int number_3 = int.Parse(Console.ReadLine()!);
-
-int max = 0;
-
-if(number_1 > number_2)
+if(x%2 == 0)
 {
-    max = number_1;
-}
-else 
-{
-    max = number_2;
-}
-
-if(number_3 > max)
-{
-    Console.WriteLine($"Из трёх чисел большее: {number_3}");
+    Console.WriteLine("Введённое число является чётным!");
 }
 else
 {
-    Console.WriteLine($"Из трёх чисел большее: {max}");
+    Console.WriteLine("Введённое число не является чётным!");
 }

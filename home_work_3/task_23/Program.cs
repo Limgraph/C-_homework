d85c710b225994534b59c96e74f92aff392b4e44
Limgraph <limgraph@mail.ru>
2023-02-11 02:08:59 +0300

    task_23

---
+++ home_work_3/task_23/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.Clear();
+
+int count = 1;
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+while (count <= a)
+{
+    Console.Write(Math.Pow(count, 3));
+    if (count != a)
+    {
+        Console.Write(", ");
+    }
+    count++;
+}

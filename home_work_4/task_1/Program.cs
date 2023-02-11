434c6ab9f0d88d946174fe10ff0bd3bed6fd4771
Limgraph <limgraph@mail.ru>
2023-02-11 16:39:20 +0300

    task_1_hw_4

---
+++ home_work_4/task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Подсчитать сумму цифр в числе
+Console. Clear();
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int sum = 0, last_num, variable = num;
+
+while(variable > 0)
+{
+    last_num = variable % 10;
+    sum += last_num;
+    variable = variable / 10;
+}
+
+Console.WriteLine($"Сумма цифр в заданном числе {num} равна: {sum}");

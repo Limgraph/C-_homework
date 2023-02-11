97551da0f0b30ce652121ef51dc7c57785b171c9
Limgraph <limgraph@mail.ru>
2023-02-11 16:51:36 +0300

    task_2_hw_4

---
+++ home_work_4/task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.Clear();
+
+Console.WriteLine("Введите до какого числа вычислить произведение чисел: ");
+int num = int.Parse(Console.ReadLine()!);
+int count = 1, multiply = 1;
+while(count <= num)
+{
+    multiply = multiply * count;
+    count++;
+}
+
+Console.WriteLine($"произведение чисел от 1 до {num} равна: {multiply}!");

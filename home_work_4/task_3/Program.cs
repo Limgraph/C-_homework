aaf22a535fdc8598610d4f75fe1dbf3b0e38b800
Limgraph <limgraph@mail.ru>
2023-02-11 17:07:19 +0300

    task_3_hw_4

---
+++ home_work_4/task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.Clear();
+
+Console.WriteLine("Введите до какого числа показать кубы чисел, заканчивающихся на четную цифру: ");
+int num = int.Parse(Console.ReadLine()!);
+
+double count = 1, cubed;
+
+while (count <= num)
+{
+    cubed = Math.Pow(count, 3);
+
+    if (cubed % 2 == 0)
+    {
+        Console.WriteLine($"Куб числа {count} = {cubed} и является чётным.");
+    }
+    count++;
+}

c03083a081c52b249f27bd70294dd5244907051f
Limgraph <limgraph@mail.ru>
2023-02-12 21:32:32 +0300

    task_34

---
+++ home_work_5/task_34/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+Console.Clear();
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+int GetEven(int[] array)
+{
+    int count_even = 0;
+    foreach (var element in array)
+    {
+        if (element % 2 == 0)
+        {
+            count_even++;
+        }
+    }
+    return count_even;
+}
+
+int[] array = GetArray(5, 100, 999);
+Console.WriteLine(String.Join(", ", array));
+
+int even = GetEven(array);
+Console.WriteLine(even);

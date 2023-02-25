ba2eedb4a3bf1c2a27f3454765d40797c62aec87
Your NameLimgraph <limgraph@mail.ru>
2023-02-25 12:55:18 +0300

    task_56

---
+++ home_work_8/task_56/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+Console.Clear();
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");//'\t' - табуляция; \n перенос на следующую строку; Escape последовательность, управляющая последовательность
+        }
+        Console.WriteLine();
+    }
+}
+
+void GetMinSum(int[,] inArray)
+{
+    int[] sumArray = new int[inArray.GetLength(0)];
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            sumArray[i] += inArray[i, j];
+        }
+    }
+    int minSum = sumArray[0];
+    int count = 0;
+    for (int i = 0; i < sumArray.Length; i++)
+    {
+        if (minSum > sumArray[i])
+        {
+            minSum = sumArray[i];
+            count = i;
+        }
+    }
+    Console.WriteLine($"Сумма элементов меньше всего в {count + 1} строке");
+}
+
+Console.Write("Введите количество строк: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов: ");
+int col = int.Parse(Console.ReadLine()!);
+
+int[,] array2D = GetArray(row, col, 1, 10);
+PrintArray(array2D);
+Console.WriteLine();
+GetMinSum(array2D);

1e812c1656e972876594693ed04589a98b3d938e
Limgraph <limgraph@mail.ru>
2023-03-07 21:14:06 +0300

    task_66

---
+++ home_work_9/task_66/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+Console.Clear();
+
+int GetSum(int M, int N)
+{
+    if (M == N) return N;
+    else return M + GetSum(M + 1, N);
+}
+Console.WriteLine("Введите первое число интервала: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число интервала: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int intervalSum = GetSum(M, N);
+Console.WriteLine(intervalSum);
